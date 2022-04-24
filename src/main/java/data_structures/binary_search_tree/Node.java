493517612459daa6893e42426d362dfcaaf01c00
aleangelozi <aleangelozi@gmail.com>
2022-04-24 17:50:01 -0300

    Binary Search Tree Data Structure

---
+++ src/main/java/data_structures/binary_search_tree/Node.java
@@ -0,0 +1,12 @@
+package data_structures.binary_search_tree;
+
+public class Node {
+
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int data) {
+        this.data = data;
+    }
+}
